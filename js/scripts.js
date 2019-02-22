7a1432cbdec64c2547f5f373bd6819e56b95bad1
Aisa <aisajemila@gmail.com>
2019-02-22 17:00:17 +0300

    Created an array for the correct answers, and defined the points for a correct answer and initialized the score to be zero.

--- js/scripts.js
+++ js/scripts.js
@@ -21,12 +21,16 @@ $(document).ready(function(){
     $("#questionnaires").show();
   }
 
+  var answers = ["method","typeof","true","selector","text()"];
+  var qstnScore = 15;
+
   $("form#questionnaires").submit(function(){
     var question1 = $("input:radio[name=question1]:checked").val();
     var question2 = $("input:radio[name=question2]:checked").val();
     var question3 = $("input:radio[name=question3]:checked").val();
     var question4 = $("input:radio[name=question4]:checked").val();
     var question5 = $("input:radio[name=question5]:checked").val();
+    var marks= 0;
 
 
 
