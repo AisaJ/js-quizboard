5d4bcdd0b023b8443d3ff8461ec420cc79fd9932
Aisa <aisajemila@gmail.com>
2019-02-22 16:24:00 +0300

    Added an onclick attribute to the start button for it to display the quiz form.

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,19 @@ $(document).ready(function(){
 
     event.preventDefault();
   });
+
+  function showQuiz(){
+    $("#questionnaires").show();
+  }
+
+  $("form#questionnaires").submit(function(){
+    var question1 = $("input:radio[name=question1]:checked").val();
+    var question2 = $("input:radio[name=question2]:checked").val();
+    var question3 = $("input:radio[name=question3]:checked").val();
+    var question4 = $("input:radio[name=question4]:checked").val();
+    var question5 = $("input:radio[name=question5]:checked").val();
+
+
+
+  })
 });
