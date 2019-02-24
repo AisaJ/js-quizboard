350e2b0862450b080ad7f3e38d3e1a9d5efd2b96
Aisa <aisajemila@gmail.com>
2019-02-24 17:16:02 +0300

    Correct an error getting user details form.

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
   $("form#user-details").submit(function(event){
 
     var firstName = $("#first-name").val();
-    var surname = $("surname").val();
-    var age = $("age").val();
+    var surname = $("#surname").val();
+    var age = $("#age").val();
     var gender = $("input:radio[name=gender]:checked").val();
 
     //UI logic
